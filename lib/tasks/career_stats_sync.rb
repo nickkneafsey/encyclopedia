cd78659a24e88fe47280b8807536421a6b6d7168
Nicholas Kneafsey <nicholaskneafsey@gmail.com>
2016-08-30 00:47:09 -0700

    Fixed ruby script to parse results

--- lib/tasks/career_stats_sync.rb
+++ lib/tasks/career_stats_sync.rb
@@ -34,49 +34,21 @@ def create_player_stats(response)
   )
 end
 
-# working
-# begin
-#   url = 'http://stats.nba.com/stats/playercareerstats?LeagueID=00&PerMode=PerGame&PlayerID=201939'
-#   uri = URI(url)
-#   res = Net::HTTP.get(uri)
-#   puts JSON.parse(res)
-# rescue => e
-#   puts e
-# end
-# def request(player_id)
-#   url = 'http://stats.nba.com/stats/playercareerstats?LeagueID=00&PerMode=PerGame&PlayerID='+player_id.to_s
-#   # uri = URI(url)
-#   RestClient.get(url){ |response, request, result, &block|
-#     if [301, 302, 307].include? response.code
-#       puts "hi"
-#       response.follow_redirection(request, result, &block)
-#     else
-#       create
-#       # response.return!(request, result, &block)
-#     end
-#   }
-# end
 
-def request(player_id)
+def read_player_file(player_id)
   begin
-    url = 'http://stats.nba.com/stats/playercareerstats?LeagueID=00&PerMode=PerGame&PlayerID=' + player_id
-    uri = URI(url)
-    puts uri
-    res = Net::HTTP.get(uri)
-    body = JSON.parse(res)
-    puts "boom"
-    # puts body
-    create_player_stats(body)
+    id = player_id.to_s
+    path = "lib/assets/#{id}.txt"
+    contents = File.readlines(path, "r")
+    parsed = JSON.parse(contents.join)
+    create_player_stats(parsed)
   rescue => e
     puts e
   end
 end
-#
-players = Player.first(3)
-# request(Player.first.nba_player_id)
-# request(76001)
+
+players = Player.all
 players.each do |player|
-  puts player.nba_player_id.to_s
-  request(player.nba_player_id.to_s)
-  sleep 60
+  player_id = player.nba_player_id.to_s
+  read_player_file(player_id)
 end
